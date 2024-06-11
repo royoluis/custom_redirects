a5c815008f21d50f5d24ce04c8e6ff7737a1fe6d
Luis Royo Antín <luisroyozgz@gmail.com>
2024-06-11 18:31:49 +0200

    Add redirection example to the EventSubscriber

--- src/EventSubscriber/CustomRedirectsSubscriber.php
+++ src/EventSubscriber/CustomRedirectsSubscriber.php
@@ -5,12 +5,14 @@ declare(strict_types=1);
 namespace Drupal\custom_redirects\EventSubscriber;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
-use Symfony\Component\HttpKernel\Event\ResponseEvent;
 use Symfony\Component\HttpKernel\KernelEvents;
+use Drupal\node\NodeInterface;
+use Drupal\Core\Routing\TrustedRedirectResponse;
 
 /**
- * @todo Add description for this subscriber.
+ * Handles custom redirects based on kernel events.
  */
 final class CustomRedirectsSubscriber implements EventSubscriberInterface {
 
@@ -18,14 +20,18 @@ final class CustomRedirectsSubscriber implements EventSubscriberInterface {
    * Kernel request event handler.
    */
   public function onKernelRequest(RequestEvent $event): void {
-    // @todo Place your code here.
-  }
+    $request = $event->getRequest();
+    $route_name = $request->attributes->get('_route');
+    $node = $request->attributes->get('node');
 
-  /**
-   * Kernel response event handler.
-   */
-  public function onKernelResponse(ResponseEvent $event): void {
-    // @todo Place your code here.
+    // Nid 2 is just an example. Set the route you wish. 
+    if ($route_name === 'entity.node.canonical' && $node instanceof NodeInterface && $node->id() == 2) {
+      user_logout();
+
+      // Drupal.org is just an example. Set the web you wish.
+      $response = new TrustedRedirectResponse('https://www.drupal.org', Response::HTTP_FOUND);
+      $event->setResponse($response);
+    }
   }
 
   /**
@@ -34,8 +40,8 @@ final class CustomRedirectsSubscriber implements EventSubscriberInterface {
   public static function getSubscribedEvents(): array {
     return [
       KernelEvents::REQUEST => ['onKernelRequest'],
-      KernelEvents::RESPONSE => ['onKernelResponse'],
     ];
   }
 
 }
+
