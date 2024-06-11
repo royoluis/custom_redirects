65454e3fb0225165f939ed837439d2abcbb5dae5
Luis Royo Antín <luisroyozgz@gmail.com>
2024-06-11 18:12:37 +0200

    Files created by 'drush generate service:event-subscriber'

---
+++ src/EventSubscriber/CustomRedirectsSubscriber.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\custom_redirects\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\Event\ResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * @todo Add description for this subscriber.
+ */
+final class CustomRedirectsSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Kernel request event handler.
+   */
+  public function onKernelRequest(RequestEvent $event): void {
+    // @todo Place your code here.
+  }
+
+  /**
+   * Kernel response event handler.
+   */
+  public function onKernelResponse(ResponseEvent $event): void {
+    // @todo Place your code here.
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents(): array {
+    return [
+      KernelEvents::REQUEST => ['onKernelRequest'],
+      KernelEvents::RESPONSE => ['onKernelResponse'],
+    ];
+  }
+
+}
