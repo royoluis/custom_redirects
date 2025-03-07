21d2b7cce4fe11a06c716cbafd6fa6f8a54a1b18
Luis Royo Antín <luisroyozgz@gmail.com>
2025-03-07 21:19:23 +0100

    Create settings form

---
+++ src/Form/CustomConfigForm.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Drupal\custom_redirects\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class CustomConfigForm extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return "custom_redirects_settings_form";
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'custom_redirects.settings'
+    ];
+  }
+
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('custom_redirects.settings');
+
+    $form['url'] = [
+      '#type' => 'textfield',
+      '#title' => t('URL to redirect after login out'),
+      '#default_value' => $config->get('url'),
+    ];
+
+    return parent::buildForm($form, $form_state);
+  }
+
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    $form_state->cleanValues();
+    $config = $this->config('custom_redirects.settings');
+    $config->set('url', $form_state->getValue('url'));
+    $config->save();
+    drupal_flush_all_caches();
+
+    parent::submitForm($form, $form_state);
+  }
+
+}
